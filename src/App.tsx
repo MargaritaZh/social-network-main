838c25c7dca4ce91d31dc45d9ee5858e4d34d7ed
MargaritaZh <margaritazhitkevich@gmail.com>
2024-06-15 23:37:58 +0300

    Social network start

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./layout/header/Header";
+
+
+
+function App() {
+
+
+    return (
+        <div>
+            <Header/>
+
+
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,28 @@
+
+import * as React from 'react';
+
+import logo from "../../assets/logo.svg"
+import {style} from "./Header.style"
+
+
+type HeaderProps = {
+    
+};
+export const Header = (props: HeaderProps) => {
+    return (
+        <style.Header>
+            <img src={logo} alt={"logotype"}/>
+            <nav>
+                <ul>
+                    <li><a href={"#"}>img</a></li>
+                    <li><a href={"#"}>img</a></li>
+                    <li><a href={"#"}>img</a></li>
+                </ul>
+                <div>
+
+                </div>
+            </nav>
+        </style.Header>
+    );
+};
+
