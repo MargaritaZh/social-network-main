5aa2d358484002fbb44eb293e9bd8afe4f4eff31
MargaritaZh <margaritazhitkevich@gmail.com>
2024-06-16 14:11:13 +0300

    GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {GlobalStyles} from "./components/styles/GlobalStyles";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <GlobalStyles/>
+        <App/>
+    </React.StrictMode>, document.getElementById('root'));
+
+
+//
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+//
+//
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+
