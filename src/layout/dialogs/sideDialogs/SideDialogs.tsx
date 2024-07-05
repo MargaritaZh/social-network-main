08e1e0ea08652ecd17942e7e24d338b508549324
MargaritaZh <margaritazhitkevich@gmail.com>
2024-07-06 00:03:27 +0300

    router dialogs, SideDialogs

--- src/layout/dialogs/sideDialogs/SideDialogs.tsx
+++ src/layout/dialogs/sideDialogs/SideDialogs.tsx
@@ -1,25 +1,36 @@
-import * as React from 'react';
 import {PersonsDialogType} from "../../../data/types";
 import {UserElement} from "../../../components/userElement/UserElement";
-import { StyleLayout } from '../../layoutStyles';
+import {StyleLayout} from '../../layoutStyles';
+import {Link, Outlet} from "react-router-dom";
 
 type SideDialogsProps = {
-    persons:Array<PersonsDialogType>
+    persons: Array<PersonsDialogType>
 };
 
 export const SideDialogs = ({persons}: SideDialogsProps) => {
     return (
-        <StyleLayout.SideBlock>
-            <h2>Диалоги</h2>
-            <ul>
-                {persons.map(person=>{
-                    return(
-                        <li key={person.id}>
-                            <UserElement name={person.name} id={person.id} smallText={person.smallText} isOnline={person.isOnline}/>
-                        </li>
-                    )
-                })}
-            </ul>
-        </StyleLayout.SideBlock>
-    );
+        <>
+
+            <StyleLayout.SideBlock>
+                <h2>Диалоги</h2>
+                <ul>
+                    {persons.map(person => {
+                        return (
+                            <Link to={'currentdialog'}>
+                                <li key={person.id}>
+                                    <UserElement name={person.name} id={person.id} smallText={person.smallText}
+                                                 isOnline={person.isOnline}/>
+                                </li>
+                            </Link>
+
+                        )
+                    })}
+                </ul>
+            </StyleLayout.SideBlock>
+            <Outlet/>
+
+
+        </>
+    )
+        ;
 };
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import {PATH} from "./PATHS.ts";
 import App from "../App.tsx";
+import {SideDialogs} from "../layout/dialogs/sideDialogs/SideDialogs.tsx";
+import {personsForDialogs} from "../data/data.ts";
+import {SideNews} from "../layout/news/sideNews/SideNews.tsx";
+import {Dialog} from "../layout/dialogs/dialog/Dialog.tsx";
 
 
 export const router = createBrowserRouter([
@@ -11,8 +15,24 @@ export const router = createBrowserRouter([
         children:[
             {
               path:PATH.DIALOGS,
-                element:<p>Content</p>,
+                element:<SideDialogs persons={personsForDialogs}/>,
+                children:[
+                    {
+                        path:'',
+                        element:<div>Диалог не выбран</div>,
+                    },
+                    {
+                        path:'currentdialog',
+                        element:<Dialog/>,
+                    },
+                ]
+
             },
+            {
+                path:PATH.NEWS,
+                element:<SideNews/>,
+            },
+
         ],
 
     }
